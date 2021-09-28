e388ee10eaa401bcccecb9126fd09f41fb12af56
kerman07 <kerman07@live.com>
2021-09-28 08:57:14 +0200

    2.14*: data for countries step3

--- part2/countries/src/components/CountryDetail.js
+++ part2/countries/src/components/CountryDetail.js
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const api_key = process.env.REACT_APP_API_KEY;
+
 const CountryDetail = ({ country }) => {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}&units=m`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      });
+  }, [country.capital]);
+
   return (
     <div>
       <h1>{country.name}</h1>
@@ -10,7 +27,27 @@ const CountryDetail = ({ country }) => {
           <li key={language.name}>{language.name}</li>
         ))}
       </ul>
-      <img src={country.flags[1]} alt="no flag found" width="300" height="200"/>
+      <img
+        src={country.flag}
+        alt="no flag found"
+        width="300"
+        height="200"
+      />
+      {Object.keys(weather).length !== 0 && (
+        <div>
+          <h2>Weather in {country.capital}</h2>
+          <div>temperature: {weather.current.temperature}&deg; C</div>
+          <img
+            src={weather.current.weather_icons[0]}
+            alt="no img"
+            width="50"
+            height="50"
+          />
+          <div>
+            wind: {weather.current.wind_speed}km/h {weather.current.wind_dir}
+          </div>
+        </div>
+      )}
     </div>
   );
 };

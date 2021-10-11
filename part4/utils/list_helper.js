c322703aff562746cd30100692450cddfc0b1ed3
kerman07 <kerman07@live.com>
2021-10-11 18:46:47 +0200

    4.4: helper functions and unit tests step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,4 +2,9 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const reducer = (suma, cur) => suma + cur.likes;
+  return blogs.reduce(reducer, 0);
+};
+
+module.exports = { dummy, totalLikes };

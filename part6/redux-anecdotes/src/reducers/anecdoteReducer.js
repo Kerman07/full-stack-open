a24c4cbc60f04a55112c3e447a23221f9e9cb688
kerman07 <kerman07@live.com>
2021-11-27 20:22:49 +0100

    6.6: anecdotes step4

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -5,6 +5,7 @@ const anecdotesAtStart = [
   "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
   "Premature optimization is the root of all evil.",
   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  "Programming isn't about what you know; it's about what you can figure out.",
 ];
 
 const getId = () => (100000 * Math.random()).toFixed(0);

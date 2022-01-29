e240d18f1294a8e42e29a394e4aa1ab6e331b91b
kerman07 <kerman07@live.com>
2022-01-29 17:58:08 +0100

    6.13: anecdotes and the backend step1

---
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/anecdotes";
+
+const getAnecdotes = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+const anecdoteService = { getAnecdotes };
+export default anecdoteService;

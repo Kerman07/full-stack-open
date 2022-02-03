959e2e0bc7a127a43ec7e52c25ede2844aa331c6
kerman07 <kerman07@live.com>
2022-02-03 14:46:08 +0100

    6.20: anecdotes and connect step2

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,16 +1,14 @@
-import { useDispatch } from "react-redux";
+import { connect } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { setNotification } from "../reducers/notificationReducer";
 
 const AnecdoteForm = (props) => {
-  const dispatch = useDispatch();
-
   const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    dispatch(createAnecdote(content));
-    dispatch(setNotification(`you created '${content}'`, 5000));
+    props.createAnecdote(content);
+    props.setNotification(`you created '${content}'`, 5000);
   };
 
   return (
@@ -26,4 +24,4 @@ const AnecdoteForm = (props) => {
   );
 };
 
-export default AnecdoteForm;
+export default connect(null, { createAnecdote, setNotification })(AnecdoteForm);
